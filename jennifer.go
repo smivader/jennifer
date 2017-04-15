e7e29293473f69e2c9f764bc65e879e971ef3742
smivader <andryusha.smirnov@icloud.com>
2017-04-15 22:31:40 +0300

    Update jennifer.go
    This is not all

--- jennifer.go
+++ jennifer.go
@@ -1,7 +1,3 @@
-// Package jennifer is a code generator for Go
 package jennifer
 
-//go:generate go get github.com/dave/jennifer/genjen
-//go:generate genjen
-//go:generate go get github.com/dave/rebecca/cmd/becca
-//go:generate becca -package=github.com/dave/jennifer/jen
+//and what you want
